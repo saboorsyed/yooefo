2793c1d7b011acb4bbd94722908d94b290ced49d
Saboor Syed <syedsaboor572@gmail.com>
2023-12-14 00:26:19 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// function brandDetailsPopup(event,brand) {
+//     const popup = document.querySelector('.pop-up-container');
+//     debugger
+//     if(popup.style.display = 'none'){
+//         popup.style.display = 'fixed';
+//         popup.style.position = 'absolute'
+//         // popup.style.top = (event.clientY - 280) + 'px'
+
+//     }
+//     else{
+//         popup.style.display = 'none';
+//         popup.style.position = 'absolute'
+//     }
+// }
+// function closeUp() {
+//     const popup = document.querySelector('.pop-up-container');
+//     popup.style.display = 'none';
+// }
