f5ca122959eb662b2b5e8af7f3e628d884745c64
Saboor Syed <syedsaboor572@gmail.com>
2024-02-14 22:35:30 +0530

    start flow changed

--- index.js
+++ index.js
@@ -35,5 +35,33 @@ function paymentPage(){
     window.location = 'bookings-page-5.html'
 }
 function homepage(){
-    window,location = 'stores.html'
+    window.location = 'stores.html'
 }
+function nextFlow(pageno){
+    if(pageno === 2){
+        window.location = '2-brand-selection.html';
+    }
+    else if(pageno === 3){
+        window.location = '3-model-selection.html';
+    }
+    else if(pageno === 4){
+        window.location = '4-services-selection.html';
+    }
+    else if(pageno === 5){
+        window.location = '5-delivery-type-selection.html'
+    }
+    else if(pageno === 6){
+        window.location = '../stores.html'
+    }
+}
+//for service popover
+function selectService(event){
+    debugger
+    let selectedEL = event.target;
+    let unselctedElement = document.getElementByClassName('model-option');
+    unselctedElement.classList.add("selected-service");
+}
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl);
+})
